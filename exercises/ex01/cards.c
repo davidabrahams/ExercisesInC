bd51866c993b4892858c5f27e01081053e00e814
David Abrahams <david.abrahams@students.olin.edu>
2018-01-29 18:34:48 -0500

    refactor our to eval_card method

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -8,6 +8,33 @@ void user_input(char* arr)
   scanf("%2s", arr);
 }
 
+
+int eval_card(char* card_name) {
+  int val = 0;
+  switch(card_name[0]) {
+    case 'K':
+    case 'Q':
+    case 'J':
+      val = 10;
+      break;
+    case 'A':
+      val = 11;
+      break;
+    case 'X':
+      val = 0;
+      break;
+    default:
+      val = atoi(card_name);
+      if ((val < 1) || (val > 10)) {
+        val = 0;
+        puts("I don't understand that value!");
+      }
+  }
+  return val;
+}
+
+
+
 int main()
 {
   char card_name[3];
@@ -15,25 +42,8 @@ int main()
   while (card_name[0] != 'x') {
     char card_name[3];
     user_input(card_name);
-    int val = 0;
-    switch(card_name[0]) {
-      case 'K':
-      case 'Q':
-      case 'J':
-        val = 10;
-        break;
-      case 'A':
-        val = 11;
-        break;
-      case 'X':
-        continue;
-      default:
-        val = atoi(card_name);
-        if ((val < 1) || (val > 10)) {
-          puts("I don't understand that value!");
-          continue;
-        }
-    }
+    int val = eval_card(card_name);
+    if (val == 0) continue;
     if ((val > 2) && (val < 7)) {
       count++;
     } else if (val == 10) {
