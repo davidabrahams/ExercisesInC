fd0cf98768fb3bd8004e76bd86a2a4b78f63644d
David Abrahams <david.abrahams@students.olin.edu>
2018-01-29 18:46:34 -0500

    add comments and address warnings

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Prompts user for a card name, and stores it in the input char* arr.
+ * Input is truncated to first 2 characters
+ *
+ * arr: a pointer to a character array to store the users input in
+ */
 void user_input(char* arr)
 {
   puts("Enter the card_name: ");
@@ -9,6 +14,10 @@ void user_input(char* arr)
 }
 
 
+/* Given a card name, return its value in the game of blackjack
+ *
+ * card_name: a pointer to a character array storing a card name
+ */
 int eval_card(char* card_name) {
   int val = 0;
   switch(card_name[0]) {
@@ -33,6 +42,9 @@ int eval_card(char* card_name) {
   return val;
 }
 
+/* Given a card value and a pointer to a count variable, updates the running
+ * "count" for the purpose of counting cards.
+ */
 void update_count(int* count, int val) {
   if (val == 0) return;
   if ((val > 2) && (val < 7)) (*count)++;
@@ -46,9 +58,10 @@ int main()
   char card_name[3];
   int count = 0;
   while (card_name[0] != 'x') {
+    int val;
     char card_name[3];
     user_input(card_name);
-    int val = eval_card(card_name);
+    val = eval_card(card_name);
     update_count(&count, val);
     printf("Current count: %i\n", count);
   }
