08c65942fff7118871e4784fcf6ff10c24f9a52c
David Abrahams <david.abrahams@students.olin.edu>
2018-03-05 22:38:43 -0500

    add random func

--- exercises/ex05/rand.c
+++ exercises/ex05/rand.c
@@ -63,6 +63,8 @@ float my_random_float2()
   }
 
   // find the location of the first set bit and compute the exponent
+  // What if this runs more than 8 times? Happens 1/256 times...
+  // seems like sometimes part of the random number gets reused.
   while (x & mask) {
     mask <<= 1;
     exp--;
@@ -78,7 +80,37 @@ float my_random_float2()
 // compute a random double using my algorithm
 double my_random_double()
 {
-  // TODO: fill this in
+  long x;
+  long mant;
+  long exp = 1022; // 2^(11-1) - 2 is an exponent of -1, b/c bias is 2^10 - 1
+  long mask = 1;
+
+  union {
+    double f;
+    long i;
+  } b;
+
+  // generate random bits until we see the first set bit
+  while (1) {
+    x = ((long) random() << 32) | random(); // generate a random 64-bit long
+    if (x == 0) {
+      exp -= 31;
+    } else {
+      break;
+    }
+  }
+
+  // find the location of the first set bit and compute the exponent
+  while (x & mask) {
+    mask <<= 1;
+    exp--;
+  }
+
+  // use the remaining bit as the mantissa
+  mant = x >> 11;
+  b.i = (exp << 52) | mant;
+
+  return b.f;
 }
 
 // return a constant (this is a dummy function for time trials)
