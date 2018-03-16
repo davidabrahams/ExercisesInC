79fa63e9381a0c235c14aca78e4685d68603d5a0
David Abrahams <david.abrahams@students.olin.edu>
2018-03-16 02:08:33 -0400

    change indentation

--- exercises/ex07/list2.c
+++ exercises/ex07/list2.c
@@ -1,8 +1,8 @@
 /* Example code for Exercises in C.
 
-Based on an example from http://www.learn-c.org/en/Linked_lists
+   Based on an example from http://www.learn-c.org/en/Linked_lists
 
-Copyright 2018 Allen Downey
+   Copyright 2018 Allen Downey
 License: Creative Commons Attribution-ShareAlike 3.0
 
 */
@@ -11,107 +11,107 @@ License: Creative Commons Attribution-ShareAlike 3.0
 #include <stdlib.h>
 
 typedef struct node {
-    int val;
-    struct node *next;
+  int val;
+  struct node *next;
 } Node;
 
 
 /* Makes a new node structure.
-*
-* val: value to store in the node.
-* next: pointer to the next node
-*
-* returns: pointer to a new node
-*/
+ *
+ * val: value to store in the node.
+ * next: pointer to the next node
+ *
+ * returns: pointer to a new node
+ */
 Node *make_node(int val, Node *next) {
-    Node *node = malloc(sizeof(Node));
-    node->val = val;
-    node->next = next;
-    return node;
+  Node *node = malloc(sizeof(Node));
+  node->val = val;
+  node->next = next;
+  return node;
 }
 
 /* Prints the values in a list.
-*
-* list: pointer to first node
-*/
+ *
+ * list: pointer to first node
+ */
 void print_list(Node *list) {
-    Node *current = list;
+  Node *current = list;
 
-    printf("[ ");
-    while (current != NULL) {
-        printf("%d ", current->val);
-        current = current->next;
-    }
-    printf("]\n");
+  printf("[ ");
+  while (current != NULL) {
+    printf("%d ", current->val);
+    current = current->next;
+  }
+  printf("]\n");
 }
 
 /* Removes the first element of a list, stores the value.
-*
-* list: pointer to first node
-  value_p: location to store the popped value
-*
-* returns: head of modified list
-*/
+ *
+ * list: pointer to first node
+value_p: location to store the popped value
+ *
+ * returns: head of modified list
+ */
 Node *pop(Node *list, int *value_p) {
-    // FILL THIS IN
-    return list;
+  // FILL THIS IN
+  return list;
 }
 
 /* Adds a new element to the beginning of the list.
-*
-* list: pointer to first node
-* value: value to add
+ *
+ * list: pointer to first node
+ * value: value to add
 
-* returns: address of modified list
-*/
+ * returns: address of modified list
+ */
 Node *push(Node *list, int value) {
-    Node *head = list;
-    Node *new_head = make_node(value, head);
-    return new_head;
+  Node *head = list;
+  Node *new_head = make_node(value, head);
+  return new_head;
 }
 
 /* Reverses the elements of the list.
-*
-* Does not allocate or free nodes.
-*
-* list: pointer to first node
+ *
+ * Does not allocate or free nodes.
+ *
+ * list: pointer to first node
 
-  returns: head of modified list
+returns: head of modified list
 */
 Node *reverse(Node *list) {
-    if (list == NULL || list->next == NULL) {
-        return list;
-    }
+  if (list == NULL || list->next == NULL) {
+    return list;
+  }
 
-    Node *current = list;
-    Node *next = current->next;
-    Node *temp;
+  Node *current = list;
+  Node *next = current->next;
+  Node *temp;
 
-    current->next = NULL;
+  current->next = NULL;
 
-    while (next != NULL) {
-        temp = next->next;
-        next->next = current;
-        current = next;
-        next = temp;
-    }
-    return current;
+  while (next != NULL) {
+    temp = next->next;
+    next->next = current;
+    current = next;
+    next = temp;
+  }
+  return current;
 }
 
 int main() {
-    Node *list = make_node(1, NULL);
-    list->next = make_node(2, NULL);
-    list->next->next = make_node(3, NULL);
+  Node *list = make_node(1, NULL);
+  list->next = make_node(2, NULL);
+  list->next->next = make_node(3, NULL);
 
-    print_list(list);
+  print_list(list);
 
-    list = push(list, 0);
-    print_list(list);
+  list = push(list, 0);
+  print_list(list);
 
-    int value;
-    list = pop(list, &value);
-    print_list(list);
+  int value;
+  list = pop(list, &value);
+  print_list(list);
 
-    list = reverse(list);
-    print_list(list);
+  list = reverse(list);
+  print_list(list);
 }
