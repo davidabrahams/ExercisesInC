935cb37b71816a707cf7cef95e7cf5722fdcab63
David Abrahams <david.abrahams@students.olin.edu>
2018-04-21 22:35:02 -0400

    Reindent

--- exercises/ex09/fork.c
+++ exercises/ex09/fork.c
@@ -1,6 +1,6 @@
 /* Example code for Exercises in C.
 
-Copyright 2016 Allen B. Downey
+   Copyright 2016 Allen B. Downey
 License: MIT License https://opensource.org/licenses/MIT
 
 */
@@ -23,17 +23,17 @@ extern int errno;
 // get_seconds returns the number of seconds since the
 // beginning of the day, with microsecond precision
 double get_seconds() {
-    struct timeval tv[1];
+  struct timeval tv[1];
 
-    gettimeofday(tv, NULL);
-    return tv->tv_sec + tv->tv_usec / 1e6;
+  gettimeofday(tv, NULL);
+  return tv->tv_sec + tv->tv_usec / 1e6;
 }
 
 
 void child_code(int i)
 {
-    sleep(i);
-    printf("Hello from child %d.\n", i);
+  sleep(i);
+  printf("Hello from child %d.\n", i);
 }
 
 // main takes two parameters: argc is the number of command-line
@@ -41,61 +41,61 @@ void child_code(int i)
 // line arguments
 int main(int argc, char *argv[])
 {
-    int status;
-    pid_t pid;
-    double start, stop;
-    int i, num_children;
+  int status;
+  pid_t pid;
+  double start, stop;
+  int i, num_children;
 
-    // the first command-line argument is the name of the executable.
-    // if there is a second, it is the number of children to create.
-    if (argc == 2) {
-        num_children = atoi(argv[1]);
-    } else {
-        num_children = 1;
+  // the first command-line argument is the name of the executable.
+  // if there is a second, it is the number of children to create.
+  if (argc == 2) {
+    num_children = atoi(argv[1]);
+  } else {
+    num_children = 1;
+  }
+
+  // get the start time
+  start = get_seconds();
+
+  for (i=0; i<num_children; i++) {
+
+    // create a child process
+    printf("Creating child %d.\n", i);
+    pid = fork();
+
+    /* check for an error */
+    if (pid == -1) {
+      fprintf(stderr, "fork failed: %s\n", strerror(errno));
+      perror(argv[0]);
+      exit(1);
     }
 
-    // get the start time
-    start = get_seconds();
+    /* see if we're the parent or the child */
+    if (pid == 0) {
+      child_code(i);
+      exit(i);
+    }
+  }
 
-    for (i=0; i<num_children; i++) {
+  /* parent continues */
+  printf("Hello from the parent.\n");
 
-        // create a child process
-        printf("Creating child %d.\n", i);
-        pid = fork();
+  for (i=0; i<num_children; i++) {
+    pid = wait(&status);
 
-        /* check for an error */
-        if (pid == -1) {
-            fprintf(stderr, "fork failed: %s\n", strerror(errno));
-            perror(argv[0]);
-            exit(1);
-        }
-
-        /* see if we're the parent or the child */
-        if (pid == 0) {
-            child_code(i);
-            exit(i);
-        }
+    if (pid == -1) {
+      fprintf(stderr, "wait failed: %s\n", strerror(errno));
+      perror(argv[0]);
+      exit(1);
     }
 
-    /* parent continues */
-    printf("Hello from the parent.\n");
+    // check the exit status of the child
+    status = WEXITSTATUS(status);
+    printf("Child %d exited with error code %d.\n", pid, status);
+  }
+  // compute the elapsed time
+  stop = get_seconds();
+  printf("Elapsed time = %f seconds.\n", stop - start);
 
-    for (i=0; i<num_children; i++) {
-        pid = wait(&status);
-
-        if (pid == -1) {
-            fprintf(stderr, "wait failed: %s\n", strerror(errno));
-            perror(argv[0]);
-            exit(1);
-        }
-
-        // check the exit status of the child
-        status = WEXITSTATUS(status);
-        printf("Child %d exited with error code %d.\n", pid, status);
-    }
-    // compute the elapsed time
-    stop = get_seconds();
-    printf("Elapsed time = %f seconds.\n", stop - start);
-
-    exit(0);
+  exit(0);
 }
